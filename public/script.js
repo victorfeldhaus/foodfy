205c4c6097c8d8064d70daa14ce77753c9e5049d
victorfeldhaus <victor.feldhaus@hotmail.com>
2020-02-09 22:23:11 -0300

    public

---
+++ public/script.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll('.card')
+
+
+for(let card of cards) {
+    card.addEventListener("click", function() {
+      const cardId = card.getAttribute("id")
+      window.location.href = `/receita?id=${cardId}`
+    })
+}
+
+
+
